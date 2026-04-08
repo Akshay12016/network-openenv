0dd1dc852c57fd8e57357a78fcc219cc3ec7c166
Akshay12016 <60560417+Akshay12016@users.noreply.github.com>
2026-04-08 22:24:43 +0530

    Add main function to run the server

--- server/app.py
+++ server/app.py
@@ -17,3 +17,11 @@ async def step(action: dict):
 @app.get("/state")
 async def state():
     return env.state()
+
+
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
